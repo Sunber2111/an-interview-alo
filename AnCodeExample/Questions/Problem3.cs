ef7c9072ea1cbf554d919a035fa177def7075077
Nam Dang (SM) <ndanghoai@deloitte.ca>
2023-04-24 11:08:25 +0700

    Resolve problem 3 4

---
+++ AnCodeExample/Questions/Problem3.cs
@@ -0,0 +1,21 @@
+﻿namespace AnCodeExample.Questions
+{
+    public class Problem3
+    {
+        public static bool Resolved(string input)
+        {
+            var len = input.Length;
+            var lenLoop = len / 2;
+
+            for (int i = 0; i < lenLoop; i++)
+            {
+                if (input[i] == input[len - i - 1])
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AnCodeExample/Questions/Problem4.cs
@@ -0,0 +1,28 @@
+﻿namespace AnCodeExample.Questions
+{
+    public static class Problem4
+    {
+        public static bool Resolved(string input)
+        {
+            var specialChars  = new HashSet<char>()
+            {
+                '#',
+                ';',
+                ',',
+                '.',
+                '?',
+                '~',
+                '$',
+                '%'
+            };
+
+            foreach (var character in input)
+            {
+                if(specialChars.Contains(character))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
