6275afa0b87cb268c2a0c7dda4b490c5ec06a658
Nam Dang (SM) <ndanghoai@deloitte.ca>
2023-04-24 10:21:45 +0700

    Resolved Problem 2

---
+++ AnCodeExample/Questions/Problem2.cs
@@ -0,0 +1,54 @@
+﻿namespace AnCodeExample.Questions
+{
+    public class Problem2
+    {
+        public static char Resolved(string input)
+        {
+            var dicChars = new Dictionary<char, long>();
+
+            foreach (char c in input)
+            {
+                if (dicChars.ContainsKey(c))
+                {
+                    dicChars[c] += 1;
+                }
+                else
+                {
+                    dicChars.Add(c, 1);
+                }
+            }
+
+            return dicChars.OrderBy(x => x.Value).FirstOrDefault().Key;
+
+        }
+
+        public static char ResolvedOptimize(string input)
+        {
+            if (String.IsNullOrEmpty(input) || String.IsNullOrWhiteSpace(input))
+                throw new Exception("Wrong input value");
+
+            if (input.Length == 1)
+                return input[0];
+
+            var dicChars = new Dictionary<char, long>();
+            var halfInputLen = input.Length % 2 == 0 ? input.Length / 2 : (input.Length / 2) + 1;
+
+            foreach (char c in input)
+            {
+                if (dicChars.ContainsKey(c))
+                {
+                    dicChars[c] += 1;
+                    if (dicChars[c] >= halfInputLen)
+                        return c;
+                }
+                else
+                {
+                    dicChars.Add(c, 1);
+                }
+            }
+
+            return dicChars.OrderBy(x => x.Value).FirstOrDefault().Key;
+
+        }
+    }
+}
